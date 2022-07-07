23a21761c9874e56752d0b4b30eb61b3f9860f04
Ravi teja <gannojuraviteja@gmail.com>
2022-07-07 12:17:45 +0530

    Moved unwanted changes to simplejavaprograms projects

--- src/main/java/com/example/Test.java
+++ src/main/java/com/example/Test.java
@@ -3,26 +3,21 @@ package com.example;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.example.service.MultipleThreads;
-
 public class Test {
 	
 	
 
 	public static void main(String[] args) {
 		
-	MultipleThreads multipleThreads = new MultipleThreads();
-	
-	multipleThreads.multithreadImplementation();
-		
-	/*
-	 * List<String> data = new ArrayList(); data.add("test"); data.add("test1");
-	 * data.forEach(System.out::println);
-	 * 
-	 * for(String record : data) {
-	 * 
-	 * }
-	 */
+	  List<String> data = new ArrayList(); 
+	  data.add("test"); 
+	  data.add("test1");
+	  data.forEach(System.out::println);
+	  
+	  for(String record : data) {
+	  
+	  }
+	 
 	}
 	
 }
