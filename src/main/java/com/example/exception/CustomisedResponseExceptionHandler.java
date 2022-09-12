a3e1e4a68e82aa958291a0908810cc825150bbda
Ravi teja <gannojuraviteja@gmail.com>
2022-09-12 17:42:41 +0530

    Added JWT token related code

--- src/main/java/com/example/exception/CustomisedResponseExceptionHandler.java
+++ src/main/java/com/example/exception/CustomisedResponseExceptionHandler.java
@@ -10,10 +10,23 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+/**
+ * 
+ * @author ganraviteja
+ *
+ *
+ *	this class is used to handle the exception that occurs at controller level.
+ *	The @ControllerAdvice is an annotation, to handle the exceptions globally.
+ */
+
 @ControllerAdvice
 @RestController
 public class CustomisedResponseExceptionHandler extends ResponseEntityExceptionHandler {
-
+	
+	/**
+	 * The @ExceptionHandler is an annotation used to handle the specific exceptions and sending the custom 
+	 * responses to the client.
+	 */
 	@ExceptionHandler(Exception.class)
 	public final ResponseEntity<Object> handleAllExceptions(Exception ex, WebRequest webRequest)  {
 		 
